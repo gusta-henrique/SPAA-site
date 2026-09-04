90a5192bf42a3c5e65394c6c3c35285bbe9c7956
gusta-henrique <henrique.gusta777@gmail.com>
2026-09-04 18:46:37 -0300

    banner finalizado

--- js/main.js
+++ js/main.js
@@ -2,5 +2,12 @@ var menuIcon = document.querySelector('.menu-icon');
 var ul = document.querySelector('.ul');
 
 menuIcon.addEventListener('click', ()=>{
-    ul.classList.add('ativo');
+
+    if(ul.classList.contains('ativo')){
+        ul.classList.remove('ativo');
+        document.querySelector('.menu-icon img').src = 'img/menu.png'
+    }else{
+        ul.classList.add('ativo');
+        document.querySelector('.menu-icon img').src = 'img/close.png'
+    }
 })
